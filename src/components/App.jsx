091d2db77a561cbbb09f235de4e291e3b0ba34b2
zndelfin <zndelfin.dev@gmail.com>
2022-07-25 22:56:21 -0400

    Phase 1: Keeper App

---
+++ src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Heading from "./Header.jsx";
+import Footer from "./Footer.jsx";
+import Note from "./Note.jsx";
+
+function App(){
+   return (<div>
+    <Heading />
+    <Note />
+    <Footer />
+   </div>);
+}
+
+export default App;
