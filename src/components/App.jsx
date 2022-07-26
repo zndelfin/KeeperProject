8ec5457cddbf856707baad998982f146559d27b2
zndelfin <zndelfin.dev@gmail.com>
2022-07-26 12:26:17 -0400

    Dynamically display Note entries and refactored code using ES6

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,11 +2,23 @@ import React from "react";
 import Heading from "./Header.jsx";
 import Footer from "./Footer.jsx";
 import Note from "./Note.jsx";
+import notes from "../Note.js";
+
+
 
 function App(){
    return (<div>
     <Heading />
-    <Note />
+    
+      {notes.map(noteItem => (
+          <Note 
+          key = {noteItem.key}
+          title = {noteItem.title}
+          content = {noteItem.content}
+       />
+       ))
+      }
+    
     <Footer />
    </div>);
 }
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function Note(){
+function Note(props){
     return (
         <div className="note"> 
-            <h1>This is the Note Title</h1>
-            <p>This is the note content</p>
+            <h1>{props.title}</h1>
+            <p>{props.content}</p>
         </div>
     );
 }
